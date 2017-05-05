16e8ca952410bb39fbae1160ed3771cffc00128c
Murilo Silva Jr <msilva12@umassd.edu>
2017-05-05 12:28:48 -0400

    wip modulation code

--- main.m
+++ main.m
@@ -19,7 +19,7 @@ joy1_enc = [bin2dec(num2str(joy1_enc(1:5))) bin2dec(num2str(joy1_enc(6:10)))]+1
 
 %%
 H = 32
-BW = 10e3; % [Hz] Bandwidth
+BW = 10e6; % [Hz] Bandwidth
 fc = 5e6 % [Hz]
 % fc = (2.4835+2.4)/2 % [Hz] Center Frequency
 % Df = (2.4835e9-2.4e9)/H % [Hz] distance between frequencies
@@ -32,9 +32,10 @@ tones = (-H/2:H/2-1)'.* Df
 
 joy1_enc_sbbf = zeros(1,fs);
 
-% joy1_enc_sbbf(tones(joy1_enc)-tones(1)+1) = ones(1,M/5);
+joy1_enc_sbbf(tones(joy1_enc)-tones(1)+1) = ones(1,M/5);
 
-% plot(joy1_enc_sbbf)
+f = (-fs/2:fs/2-1);
+plot(f,fftshift(joy1_enc_sbbf))
 
 
 %% FSK Modulator
